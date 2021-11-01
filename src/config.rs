ed7a6b3cbe8d804b8cf185b76556745f6c6c573c
maxfer1221 <maxfer1221@gmail.com>
2021-11-01 05:05:37 -0400

    add ability to locate/create config file. config struct can be written to file but cannot be read from file yet. error handling is partially done

--- src/config.rs
+++ src/config.rs
@@ -1,37 +1,95 @@
-//use std::{fs, path::PathBuf};
+use std::io::{self, BufReader, prelude::*};
+use std::fs::File;
 use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
 //use toml;
 //use dirs_next;
 
-
 #[derive(Serialize, Deserialize)]
-struct Config {
-    dir: String,
+pub struct Config {
+    trash_dir: String,
 }
 
-pub fn create_config() {
-    let config_loc: Option<PathBuf> = dirs_next::config_dir();
-    let mut app_conf:   PathBuf = PathBuf::new();
-    match config_loc {
+impl Config {
+    fn new() -> Config {
+        return Config { trash_dir: String::new() };
+    }
+}
+
+pub fn create_config() -> Config {
+    let (conf_loc, trash_loc) = resolve_dirs();
+    let mut config: Config = Config::new();
+
+    let mut conf_file = File::open(&conf_loc);
+    
+    match conf_file {
+        Err(file_open_error) => {
+            println!("File not found. Creating configuration file.");     
+            conf_file = File::create(&conf_loc);
+            let mut conf_file = match conf_file {
+                Err(file_create_error) => {
+                    panic!("Unable to create configuration file: {:?}", file_create_error);
+                }
+                Ok(file) => file
+            };
+
+            config = Config { trash_dir: trash_loc.into_os_string().into_string().unwrap() };
+            let toml_as_str: String = toml::to_string(&config).unwrap();
+            let bytes: &[u8] = (toml_as_str).as_bytes();
+            conf_file.write(bytes);
+            return config;
+        }
+        Ok(file) => {
+            println!("{:?}", file);
+
+            let mut buf_reader = BufReader::new(file);
+            let mut contents = String::new();
+
+            buf_reader.read_to_string(&mut contents);
+            println!("File contents: {}", contents);
+
+            config = Config { trash_dir: String::new() };
+            return config;
+        }
+    }
+}
+
+fn resolve_dirs() -> (PathBuf, PathBuf)  {
+    let     conf_temp: Option<PathBuf> = dirs_next::config_dir();
+    let mut conf_true: PathBuf;
+    match conf_temp {
         Some(buf) => {
-            app_conf = buf;
-            println!("{:?}", app_conf);
+            conf_true = buf;
+            conf_true.push("trash-rs");
+            conf_true.push("config");
+            conf_true.set_extension("toml");
         }
         None => {
-            println!("Config location unkown. Exiting.");
+            println!("OS \"config\" location unkown. Please specify a directory to hold configuration files.");
+            let mut str_to_buf = String::new();
+            io::stdin().read_line(&mut str_to_buf);
+            conf_true = PathBuf::from(str_to_buf);
         }
-    };
-    app_conf.push("trash-rs");
-    app_conf.push("config");
-    app_conf.set_extension("toml");
-    /*
-    if let fs::read_to_string(config_loc) = Ok(String { vec: val }) {
-        match toml::from_str(fs::read_to_string(config_loc)) { 
-            Ok(config) => println!("{:?}", config);
-            Err(e) => return Config { dir: "" };
+    }
+    
+    let     trash_temp: Option<PathBuf> = dirs_next::home_dir();
+    let mut trash_true: PathBuf;
+    match trash_temp {
+        Some(buf) => {
+            trash_true = buf;
+            trash_true.push(".trash-rs");
         }
-    };
-    */
+        None => {
+            println!("OS \"home\" locatpion unkown. Please specify a destination for removed files.");
+            let mut str_to_buf = String::new();
+            let read_result = io::stdin().read_line(&mut str_to_buf);
+            match read_result {
+                Err(io_err) => panic!("Failed to read input: {:?}", io_err),
+                _ => {}
+            }
+            trash_true = PathBuf::from(str_to_buf);
+        }
+    }
+    
+    (conf_true, trash_true)
 }
-
